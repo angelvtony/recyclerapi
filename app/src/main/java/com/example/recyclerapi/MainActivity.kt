40d1541cfef8c8dcac325b26476b726438908546
Angel <angelvtony2001@gmail.com>
2023-10-18 12:01:36 +0530

    Recycler view with api integrated

--- app/src/main/java/com/example/recyclerapi/MainActivity.kt
+++ app/src/main/java/com/example/recyclerapi/MainActivity.kt
@@ -3,19 +3,25 @@ package com.example.recyclerapi
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val loading = findViewById<ProgressBar>(R.id.progress_bar)
         lifecycleScope.launch {
             loading.visibility = View.VISIBLE
--- app/src/main/java/com/example/recyclerapi/UserAdapter.kt
+++ app/src/main/java/com/example/recyclerapi/UserAdapter.kt
@@ -9,34 +9,41 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class UserAdapter(private val listener : (User) -> Unit) : ListAdapter<User, UserAdapter.ViewHolder>(DiffUserCallBack()){
+class UserAdapter(private val listener: (User) -> Unit) :
+    ListAdapter<User, UserAdapter.ViewHolder>(DiffUserCallBack()) {
 
-    inner class ViewHolder(private val containerView : View) : RecyclerView.ViewHolder(containerView){
+    inner class ViewHolder(private val containerView: View) :
+        RecyclerView.ViewHolder(containerView) {
         init {
             itemView.setOnClickListener {
                 listener.invoke(getItem(adapterPosition))
             }
         }
 
-        fun bind(user : User){
+        fun bind(user: User) {
             containerView.findViewById<TextView>(R.id.country).text = user.Country
 //            containerView.findViewById<TextView>(R.id.commonName).text = user.Mfr_CommonName
 //            containerView.findViewById<TextView>(R.id.mfrid).text = user.Mfr_ID
             containerView.findViewById<TextView>(R.id.mfrname).text = user.Mfr_Name
         }
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemLayout = LayoutInflater.from(parent.context).inflate(R.layout.user_layout, parent, false)
+        val itemLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.user_layout, parent, false)
         return ViewHolder(itemLayout)
     }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 }
-class DiffUserCallBack : DiffUtil.ItemCallback<User>(){
+
+class DiffUserCallBack : DiffUtil.ItemCallback<User>() {
     override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
         return oldItem.Country == newItem.Country
     }
+
     override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
         return oldItem == newItem
     }
